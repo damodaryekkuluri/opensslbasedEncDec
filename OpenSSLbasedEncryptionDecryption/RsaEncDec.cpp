79275c3659e37f6eb9145c3fb5cf8ce327fd31ba
damodaryekkuluri <damodar3sachin@gmail.com>
2021-05-19 18:41:43 -0400

    working version

--- OpenSSLbasedEncryptionDecryption/RsaEncDec.cpp
+++ OpenSSLbasedEncryptionDecryption/RsaEncDec.cpp
@@ -1,4 +1,7 @@
 #include <string>
+#include <Windows.h>
+#include <vector>
+#include <iostream>
 
 #include <openssl/pem.h>
 #include <openssl/ssl.h>
@@ -8,7 +11,11 @@
 #include <openssl/err.h>
 #include <stdio.h>
 
-int padding = RSA_PKCS1_PADDING;
+#include "Base64Encode.h"
+
+using namespace std;
+
+int padding = RSA_PKCS1_OAEP_PADDING;
 
 RSA * createRSA(unsigned char * key, int ispublic)
 {
@@ -20,14 +27,8 @@ RSA * createRSA(unsigned char * key, int ispublic)
 		printf("Failed to create key BIO");
 		return 0;
 	}
-	if (ispublic)
-	{
-		rsa = PEM_read_bio_RSA_PUBKEY(keybio, &rsa, NULL, NULL);
-	}
-	else
-	{
-		rsa = PEM_read_bio_RSAPrivateKey(keybio, &rsa, NULL, NULL);
-	}
+	
+	rsa = PEM_read_bio_RSAPrivateKey(keybio, &rsa, NULL, NULL);
 	if (rsa == NULL)
 	{
 		printf("Failed to create RSA");
@@ -36,30 +37,52 @@ RSA * createRSA(unsigned char * key, int ispublic)
 	return rsa;
 }
 
-int public_encrypt(unsigned char * data, int data_len, unsigned char * key, unsigned char *encrypted)
-{
-	RSA * rsa = createRSA(key, 1);
-	int result = RSA_public_encrypt(data_len, data, encrypted, rsa, padding);
-	return result;
+int private_decrypt(unsigned char * enc_data, int data_len, unsigned char * key, unsigned char *decrypted){
+
+	RSA * rsa = createRSA(key, 0);
+	EVP_PKEY    *privKey = NULL;
+	BIO         *bioPrivKey;
+	size_t         outLen = 0, ret;
+
+	if ((bioPrivKey = BIO_new(BIO_s_mem())))
+	{
+		// Read the private key from the RSA context into the memory BIO,
+		// then convert it to an EVP_PKEY:
+		if ((ret = PEM_write_bio_RSAPrivateKey(bioPrivKey, rsa, NULL, NULL, 0, NULL, NULL)) &&
+			(privKey = PEM_read_bio_PrivateKey(bioPrivKey, NULL, NULL, NULL)))
+		{
+			EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new(privKey, NULL);
+
+			EVP_PKEY_free(privKey);
+
+			if (ctx)
+			{
+				if (EVP_PKEY_decrypt_init(ctx) > 0)
+				{
+					EVP_PKEY_CTX_ctrl_str(ctx, "rsa_padding_mode", "oaep");
+					EVP_PKEY_CTX_ctrl_str(ctx, "rsa_oaep_md", "sha256");
+					EVP_PKEY_CTX_ctrl_str(ctx, "rsa_mgf1_md", "sha256");
+
+					outLen = 4098;
+					ret = EVP_PKEY_decrypt(ctx, decrypted, &outLen, enc_data, data_len);
+					if (ret > 0 && outLen > 0 && outLen <= 4098)
+					{
+						// Success :-)
+					}
+				}
+				EVP_PKEY_CTX_free(ctx);
+			}
+		}
+
+		BIO_free_all(bioPrivKey);
+	}
+	return ret;
 }
-int private_decrypt(unsigned char * enc_data, int data_len, unsigned char * key, unsigned char *decrypted)
+
+int private_decrypt1(unsigned char * enc_data, int data_len, unsigned char * key, unsigned char *decrypted)
 {
 	RSA * rsa = createRSA(key, 0);
 	int  result = RSA_private_decrypt(data_len, enc_data, decrypted, rsa, padding);
-	return result;
-}
-
-
-int private_encrypt(unsigned char * data, int data_len, unsigned char * key, unsigned char *encrypted)
-{
-	RSA * rsa = createRSA(key, 0);
-	int result = RSA_private_encrypt(data_len, data, encrypted, rsa, padding);
-	return result;
-}
-int public_decrypt(unsigned char * enc_data, int data_len, unsigned char * key, unsigned char *decrypted)
-{
-	RSA * rsa = createRSA(key, 1);
-	int  result = RSA_public_decrypt(data_len, enc_data, decrypted, rsa, padding);
 	return result;
 }
 
@@ -72,84 +95,57 @@ void printLastError(const char *msg)
 	free(err);
 }
 
+bool ReadDataFromFile(const wstring& readFile, vector<BYTE>& fileContent)
+{
+	HANDLE hFile = CreateFile(readFile.c_str(),
+		GENERIC_READ,
+		0,
+		NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		printf("Unable to open file \"%ws\" for read with %ld.", readFile.c_str(), GetLastError());
+		return false;
+	}
+
+	DWORD dwFileSize = GetFileSize(hFile, NULL);
+	fileContent.resize(dwFileSize);
+
+	DWORD dwBytesRead = 0;
+
+	bool bResult = ReadFile(hFile, fileContent.data(), dwFileSize, &dwBytesRead, NULL);
+	if (!bResult) {
+		printf("Read file \"%ws\" failed with error:%ld.", readFile.c_str(), GetLastError());
+	}
+
+	CloseHandle(hFile);
+	return bResult;
+}
+
 int RsaMain() {
 
-	char plainText[2048 / 8] = "Hello this is Ravi"; //key length : 2048
+	char plainText[2048 / 8] = "Hj0KKRoMPSUFLzoaMTYnLhg3BhEOPD47Pj80OSU6HDI="; //key length : 2048
 
-	char publicKey[] = "-----BEGIN PUBLIC KEY-----\n"\
-		"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAy8Dbv8prpJ/0kKhlGeJY\n"\
-		"ozo2t60EG8L0561g13R29LvMR5hyvGZlGJpmn65+A4xHXInJYiPuKzrKUnApeLZ+\n"\
-		"vw1HocOAZtWK0z3r26uA8kQYOKX9Qt/DbCdvsF9wF8gRK0ptx9M6R13NvBxvVQAp\n"\
-		"fc9jB9nTzphOgM4JiEYvlV8FLhg9yZovMYd6Wwf3aoXK891VQxTr/kQYoq1Yp+68\n"\
-		"i6T4nNq7NWC+UNVjQHxNQMQMzU6lWCX8zyg3yH88OAQkUXIXKfQ+NkvYQ1cxaMoV\n"\
-		"PpY72+eVthKzpMeyHkBn7ciumk5qgLTEJAfWZpe4f4eFZj/Rc8Y8Jj2IS5kVPjUy\n"\
-		"wQIDAQAB\n"\
-		"-----END PUBLIC KEY-----\n";
+	wstring pKeyFile = L"C:\\Users\\Dhamu\\Desktop\\DummyProjects\\OpenSSLbasedEncryptionDecryption\\k8s-key.pem";
+	vector<BYTE> pKey;
+	if (!ReadDataFromFile(pKeyFile, pKey)) {
+		cout << "TOO BAD";
+	}
 
-	char privateKey[] = "-----BEGIN RSA PRIVATE KEY-----\n"\
-		"MIIEowIBAAKCAQEAy8Dbv8prpJ/0kKhlGeJYozo2t60EG8L0561g13R29LvMR5hy\n"\
-		"vGZlGJpmn65+A4xHXInJYiPuKzrKUnApeLZ+vw1HocOAZtWK0z3r26uA8kQYOKX9\n"\
-		"Qt/DbCdvsF9wF8gRK0ptx9M6R13NvBxvVQApfc9jB9nTzphOgM4JiEYvlV8FLhg9\n"\
-		"yZovMYd6Wwf3aoXK891VQxTr/kQYoq1Yp+68i6T4nNq7NWC+UNVjQHxNQMQMzU6l\n"\
-		"WCX8zyg3yH88OAQkUXIXKfQ+NkvYQ1cxaMoVPpY72+eVthKzpMeyHkBn7ciumk5q\n"\
-		"gLTEJAfWZpe4f4eFZj/Rc8Y8Jj2IS5kVPjUywQIDAQABAoIBADhg1u1Mv1hAAlX8\n"\
-		"omz1Gn2f4AAW2aos2cM5UDCNw1SYmj+9SRIkaxjRsE/C4o9sw1oxrg1/z6kajV0e\n"\
-		"N/t008FdlVKHXAIYWF93JMoVvIpMmT8jft6AN/y3NMpivgt2inmmEJZYNioFJKZG\n"\
-		"X+/vKYvsVISZm2fw8NfnKvAQK55yu+GRWBZGOeS9K+LbYvOwcrjKhHz66m4bedKd\n"\
-		"gVAix6NE5iwmjNXktSQlJMCjbtdNXg/xo1/G4kG2p/MO1HLcKfe1N5FgBiXj3Qjl\n"\
-		"vgvjJZkh1as2KTgaPOBqZaP03738VnYg23ISyvfT/teArVGtxrmFP7939EvJFKpF\n"\
-		"1wTxuDkCgYEA7t0DR37zt+dEJy+5vm7zSmN97VenwQJFWMiulkHGa0yU3lLasxxu\n"\
-		"m0oUtndIjenIvSx6t3Y+agK2F3EPbb0AZ5wZ1p1IXs4vktgeQwSSBdqcM8LZFDvZ\n"\
-		"uPboQnJoRdIkd62XnP5ekIEIBAfOp8v2wFpSfE7nNH2u4CpAXNSF9HsCgYEA2l8D\n"\
-		"JrDE5m9Kkn+J4l+AdGfeBL1igPF3DnuPoV67BpgiaAgI4h25UJzXiDKKoa706S0D\n"\
-		"4XB74zOLX11MaGPMIdhlG+SgeQfNoC5lE4ZWXNyESJH1SVgRGT9nBC2vtL6bxCVV\n"\
-		"WBkTeC5D6c/QXcai6yw6OYyNNdp0uznKURe1xvMCgYBVYYcEjWqMuAvyferFGV+5\n"\
-		"nWqr5gM+yJMFM2bEqupD/HHSLoeiMm2O8KIKvwSeRYzNohKTdZ7FwgZYxr8fGMoG\n"\
-		"PxQ1VK9DxCvZL4tRpVaU5Rmknud9hg9DQG6xIbgIDR+f79sb8QjYWmcFGc1SyWOA\n"\
-		"SkjlykZ2yt4xnqi3BfiD9QKBgGqLgRYXmXp1QoVIBRaWUi55nzHg1XbkWZqPXvz1\n"\
-		"I3uMLv1jLjJlHk3euKqTPmC05HoApKwSHeA0/gOBmg404xyAYJTDcCidTg6hlF96\n"\
-		"ZBja3xApZuxqM62F6dV4FQqzFX0WWhWp5n301N33r0qR6FumMKJzmVJ1TA8tmzEF\n"\
-		"yINRAoGBAJqioYs8rK6eXzA8ywYLjqTLu/yQSLBn/4ta36K8DyCoLNlNxSuox+A5\n"\
-		"w6z2vEfRVQDq4Hm4vBzjdi3QfYLNkTiTqLcvgWZ+eX44ogXtdTDO7c+GeMKWz4XX\n"\
-		"uJSUVL5+CVjKLjZEJ6Qc2WZLl94xSwL71E41H4YciVnSCQxVc4Jw\n"\
-		"-----END RSA PRIVATE KEY-----\n";
+	string encryptedBase64 = "jRJ3Lmy1/5dsJP1sDNTCIPcSN4V41GnBqfCZV7s7w6XHEKCupbUJPodkRwaA2Ff+acyFYzQjapJk4S2pX+YL6G45fOqmxgH1fNBR0g01JIEGqRMCxhNRRuYZJ/AtvMrPhAMfYBRphMlW9rEiOrn3+4rm0kr6wav+I98NuLvQtTu5nWKeXbgbOUlFvTdX+ZNRENFSpOmRZ5WhM4btTzPI/F1MUOXdmHxoU7//BXfYyLM457prwc3aQxmO/AWyHYPfkoMu9h5wPaQdID0BCCSUpG7DbvVkSoqSxSgZZse8Og6t/zaRbUCLq+WIzYuBEWfrqzxjStvJoHXz4jf3GzGo0DoNBMIsHXOsej7vFx7Ts6f25yOIU9HJll2QrjuSGuEnyOx+X7DRXzDKL5IC/pSbe+JgGMlN2XQ7vFqLLmwEA/kwtcCxuv6d2cA36C8dIEJ4WQdBMNH60SrG62hZs/HQf0g8NrcNwBZ5WyVkZz8w6GbzSOCisvLSpsu88gV0yo9H+LBEn56TSmkfjJT1nbR/FIiweMMjV4pqJAdImJ2lxdmU29Ca8AhukGRvygtT6K1edOAxhyDTbiF4vQSeyKCCfy6lXHMo/U3fTjNGBxp/Ew4uwhmPB7t5FyyiY6Qv4/2sRw8F0yW4+KcMe0ofI8bIW6BioYw7YxR1mo5db7ZvZdA=";
+	std::vector<unsigned char> encrypted = Base64Decode(encryptedBase64);
 
-
-	unsigned char  encrypted[4098] = {};
 	unsigned char decrypted[4098] = {};
 
-	int encrypted_length = public_encrypt((unsigned char *)plainText, strlen(plainText), (unsigned char *)publicKey, encrypted);
-	if (encrypted_length == -1)
-	{
-		printLastError("Public Encrypt failed ");
-		exit(0);
-	}
-	printf("Encrypted length =%d\n", encrypted_length);
-
-	int decrypted_length = private_decrypt(encrypted, encrypted_length, (unsigned char *)privateKey, decrypted);
+	int decrypted_length = private_decrypt((unsigned char *)encrypted.data(), encrypted.size(), (unsigned char *)pKey.data(), decrypted);
 	if (decrypted_length == -1)
 	{
 		printLastError("Private Decrypt failed ");
 		exit(0);
 	}
-	printf("Decrypted Text =%s\n", decrypted);
-	printf("Decrypted Length =%d\n", decrypted_length);
 
-
-	encrypted_length = private_encrypt((unsigned char *)plainText, strlen(plainText), (unsigned char *)privateKey, encrypted);
-	if (encrypted_length == -1)
-	{
-		printLastError("Private Encrypt failed");
-		exit(0);
-	}
-	printf("Encrypted length =%d\n", encrypted_length);
-
-	decrypted_length = public_decrypt(encrypted, encrypted_length, (unsigned char *)publicKey, decrypted);
-	if (decrypted_length == -1)
-	{
-		printLastError("Public Decrypt failed");
-		exit(0);
-	}
-	printf("Decrypted Text =%s\n", decrypted);
-	printf("Decrypted Length =%d\n", decrypted_length);
+	printf("Decrypted Text: %s\n", decrypted);
 }
